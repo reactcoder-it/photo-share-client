6f28fa84227e410262c658dc6d8e63556bc50bf2
Vadim Pashaev <vmp@live.ru>
2020-01-12 10:51:12 +0400

    v2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient, { InMemoryCache } from 'apollo-boost'
+import { persistCache } from 'apollo-cache-persist'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache()
+persistCache({ cache, storage: localStorage })
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (localStorage['apollo-cache-persist']) {
+  let cacheData = JSON.parse(localStorage['apollo-cache-persist'])
+  cache.restore(cacheData)
+}
+
+const client = new ApolloClient({
+  cache,
+  uri: 'http://localhost:4000/graphql',
+  request: operation => {
+    operation.setContext(context => ({
+      headers: {
+        ...context.headers,
+        authorization: localStorage.getItem('token')
+      }
+    }))
+  }
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+, document.getElementById('root'))
